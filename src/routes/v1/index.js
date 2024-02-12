d5d0ae59bf25e53b07b9b0525ff832035383ef85
Aniket <aniketraj28042003@gmail.com>
2024-02-12 23:17:35 +0530

    added rabbitmq config

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const { BookingControllers } = require("../../controllers/index");
 
 router.post("/bookings", BookingControllers.create);
+router.post("/publishing", BookingControllers.sendMessageQueue);
 
 module.exports = router;
