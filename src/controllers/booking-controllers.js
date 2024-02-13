3b01d2387b1f775978ed5038231b54452d235718
Aniket <aniketraj28042003@gmail.com>
2024-02-13 19:39:53 +0530

    added logics to message queue

--- src/controllers/booking-controllers.js
+++ src/controllers/booking-controllers.js
@@ -8,8 +8,16 @@ const bookingService = new BookingService();
 
 const sendMessageQueue = async(req, res) => {
     const channel = await createChannel();
-    const data = {message: "Success"};
-    publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(data));
+    const payload = {
+        data: {
+            subject: "This is a noti from queue",
+            content: "Some queue will subscribe this",
+            recepientEmail: "aniketraj28042003@gmail.com",
+            notificationTime: " 2024-02-14 05:50:20"
+        },
+        service: "CREATE_TICKET"
+    };
+    publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(payload));
     return res.status(200).json({
         message: "Successfully published the event"
     });
